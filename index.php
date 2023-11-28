5bad3e6e6ffe1b09897d436b6e1acf7f064b5f12
matteobonelli <matteobonelli08@gmail.com>
2023-11-28 16:39:49 +0100

    snack 3 completed

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
-<form method="GET" action="snack2.php">
-    <div class="mb-3">
-        <label for="nome" class="form-label">Nome</label>
-        <input type="text" class="form-control" id="nome" name="nome">
-    </div>
-    <div class="mb-3">
-        <label for="mail" class="form-label">Email</label>
-        <input type="text" class="form-control" name="email" id="email">
-    </div>
-    <div class="mb-3 form-check">
-        <label class="form-check-label" for="age">Età</label>
-        <input type="text" class="form-check-input" name="age" id="age">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body class="container w-25">
+    <form method="GET" action="snack2.php">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome">
+        </div>
+        <div class="mb-3">
+            <label for="mail" class="form-label">Email</label>
+            <input type="text" class="form-control" name="email" id="email">
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="age">Età</label>
+            <input type="text" class="form-control" name="age" id="age">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
